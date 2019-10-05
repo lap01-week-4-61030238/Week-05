90e225af7e2f9b7150c884ad2b2479d0b694eb8e
“Saya” <61030238@kmitl.ac.th>
2019-10-05 16:12:07 +0700

    11

--- 61030238week5(2)/ConsoleApp1/ConsoleApp1/Program.cs
+++ 61030238week5(2)/ConsoleApp1/ConsoleApp1/Program.cs
@@ -6,19 +6,19 @@ namespace ConsoleApp1
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Line 1");
-            Console.WriteLine("Line 2");
-            Console.WriteLine("Line 3");
-        line4:
-            Console.WriteLine("Line 4");
-            Console.WriteLine("Line 5");
-            Console.WriteLine("Line 6");
-            goto line10;
-            Console.WriteLine("Line 7");
-            Console.WriteLine("Line 8");
-            Console.WriteLine("Line 9");
-        line10:
-            Console.WriteLine("Line 10");
+            int a = 10;
+            int b = 20;
+            if (a == 10)
+            {
+                if (b == 20)
+                {
+                    Console.WriteLine("a = 10 and b = 20");
+                }
+                if (b != 20)
+                {
+                    Console.WriteLine("a = 10 and b != 20");
+                }
+            }
         }
     }
 }
