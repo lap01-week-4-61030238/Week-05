b5cbd8210749b7fe7c1238910d0ee353535f0c08
“Saya” <61030238@kmitl.ac.th>
2019-10-05 17:06:11 +0700

    25

--- 61030238week5(2)/ConsoleApp1/ConsoleApp1/Program.cs
+++ 61030238week5(2)/ConsoleApp1/ConsoleApp1/Program.cs
@@ -6,19 +6,39 @@ namespace ConsoleApp1
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Line 1");
-            Console.WriteLine("Line 2");
-            Console.WriteLine("Line 3");
-        line4:
-            Console.WriteLine("Line 4");
-            Console.WriteLine("Line 5");
-            Console.WriteLine("Line 6");
-            goto line10;
-            Console.WriteLine("Line 7");
-            Console.WriteLine("Line 8");
-            Console.WriteLine("Line 9");
-        line10:
-            Console.WriteLine("Line 10");
+            // Initial string array with month names.
+            string[] monthName = new string[] { "January","February",
+            "March","April","May","June","July","August",
+            "September","October","November","December"};
+
+            Console.WriteLine("-------- foreach,in loop");
+            foreach (string month in monthName)
+            {
+                System.Console.WriteLine(month);
+            }
+            System.Console.WriteLine();
+
+            // Compare the previous loop to a similar for loop.
+            Console.WriteLine("-------- for loop");
+            for (int i = 0; i < monthName.Length; i++)
+            {
+                System.Console.WriteLine(monthName[i]);
+            }
+            System.Console.WriteLine();
+
+
+            // You can maintain a count of the elements in the collection.
+            int count = 0;
+            foreach (string month in monthName)
+            {
+                count += 1;
+                System.Console.WriteLine("Element #{0}: {1}", count, month);
+            }
+            System.Console.WriteLine("Number of elements in the array: {0}", count);
+
+            // Keep the console open in debug mode.
+            Console.WriteLine("Press any key to exit.");
+            Console.ReadKey();
         }
     }
 }
