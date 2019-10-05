1897f8686f3b58e0fe5e46f16e63be27d3c3fac6
“Saya” <61030238@kmitl.ac.th>
2019-10-05 16:50:58 +0700

    20

--- 61030238week5(2)/ConsoleApp1/ConsoleApp1/Program.cs
+++ 61030238week5(2)/ConsoleApp1/ConsoleApp1/Program.cs
@@ -6,19 +6,10 @@ namespace ConsoleApp1
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Line 1");
-            Console.WriteLine("Line 2");
-            Console.WriteLine("Line 3");
-        line4:
-            Console.WriteLine("Line 4");
-            Console.WriteLine("Line 5");
-            Console.WriteLine("Line 6");
-            goto line10;
-            Console.WriteLine("Line 7");
-            Console.WriteLine("Line 8");
-            Console.WriteLine("Line 9");
-        line10:
-            Console.WriteLine("Line 10");
+            for (int i = 1; i <= 12; i++)
+            {
+                Console.WriteLine("{0, 2} x 2 = {1, 2}", i, i * 2);
+            }
         }
     }
 }
