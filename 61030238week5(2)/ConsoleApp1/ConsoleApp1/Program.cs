1322e59fd433545f1e908e562593c3a90e63f217
“Saya” <61030238@kmitl.ac.th>
2019-10-05 14:34:45 +0700

    switch

--- 61030238week5(2)/ConsoleApp1/ConsoleApp1/Program.cs
+++ 61030238week5(2)/ConsoleApp1/ConsoleApp1/Program.cs
@@ -6,19 +6,34 @@ namespace ConsoleApp1
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Line 1");
-            Console.WriteLine("Line 2");
-            Console.WriteLine("Line 3");
-        line4:
-            Console.WriteLine("Line 4");
-            Console.WriteLine("Line 5");
-            Console.WriteLine("Line 6");
-            goto line10;
-            Console.WriteLine("Line 7");
-            Console.WriteLine("Line 8");
-            Console.WriteLine("Line 9");
-        line10:
-            Console.WriteLine("Line 10");
+            Console.WriteLine("Coffee sizes: 1=Small 2=Medium 3=Large");
+            Console.Write("Please enter your selection: ");
+            string s = Console.ReadLine();
+            int n = int.Parse(s);
+            int cost = 0;
+            switch (n)
+            {
+                case 1:
+                    cost += 25;
+                    break;
+                case 2:
+                    cost += 25;
+                    goto case 1;
+                case 3:
+                    cost += 50;
+                    goto case 1;
+                default:
+                    Console.WriteLine("Invalid selection.");
+                    break;
+            }
+            if (cost != 0)
+            {
+                Console.WriteLine("Please insert {0} Bath.", cost);
+            }
+            Console.WriteLine("Thank you for your business.");
+            // Keep the console open in debug mode.
+            Console.WriteLine("Press any key to exit.");
+            Console.ReadKey();
         }
     }
 }
